1dfa7a7bb27e3d5be1fe99cd69e705875d4c9e83
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-06-17 06:39:55 +0545

    Update and rename array 1.c to printing the elements of a user defined array.c

--- printing the elements of a user defined array.c
+++ printing the elements of a user defined array.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 
 int main(){
-	int num[8] = 0;
-	int i = 0;
+	int num[8]= {0};
+	int i = 0, sum=0;
 	while (i<=7){
 		printf("Enter elements for the array: ");
 		scanf("%d", &num[i]);
+		sum += num[i];
 		i += 1;
-		printf("%d\n", num[i]);
 	}
+	for(i=0;i<=7;i++){
+		printf("%d\n", &num[i]);
+	}
+	printf("\nSum is %d", sum);
 	return 0;
 }
